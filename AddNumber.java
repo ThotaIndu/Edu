53515c81f411dc6784f8ded148deb9f130f4adfe
ThotaIndu <indureddythota@gmail.com>
2024-11-19 16:21:13 +0530

    indu

---
+++ AddNumber.java
@@ -0,0 +1,10 @@
+class AddNumber
+{
+public static void main(String args[])
+{
+double number1=12345.12;
+double number2=22222.12;
+double result=number1+number2;
+System.out.println("Adding two numbers "+result);
+}
+}
