0f8ec8303c757e0844881270409fd70bd4c1b114
ThotaIndu <indureddythota@gmail.com>
2024-11-19 16:27:47 +0530

    indu1

---
+++ AreaofSquare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class AreaofSquare{
+ public static int Square(int a){
+ return a*a;
+ }
+ public static void main(String[] args){
+ Scanner sc = new Scanner(System.in);
+ int a=sc.nextInt();
+ Square(a);
+ System.out.println("Area of square: " +area);
+ sc.close();
+ }
+}
